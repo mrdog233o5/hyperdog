bfebb2ba0a14183be7d8349599971a168ad990cd
mrdog233o5 <mr.dog233o5@gmail.com>
2023-09-18 07:29:21 +0800

    basic window size change done

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 #!/opt/homebrew/bin/python3
 import subprocess
+import pyautogui
+from pynput import keyboard
 
-# Define the AppleScript code to set the size of a Finder window
-applescript_code = '''
-tell application "Finder"
-    activate -- Bring Finder to the front
-end tell
-
-tell application "System Events"
-    tell process "Finder"
-        set frontmost to true
-        tell window 1
-            set size to {800, 600} -- New size for the window
-            set position to {100, 100} -- New position for the window
+keyPressed = {}
+width = pyautogui.size().width
+height = pyautogui.size().height
+ 
+def moveWindow(x,y):
+    subprocess.run(['osascript', '-e', f'''
+    tell application "System Events"
+        set frontApp to name of first application process whose frontmost is true
+        tell process frontApp
+            set frontWindow to first window
+            set position of frontWindow to {x, y} -- New position for the window
         end tell
-    end tell
-end tell
-'''
+    end tell'''])
 
-# Execute the AppleScript code using osascript
-subprocess.run(['osascript', '-e', applescript_code])
+def resizeWindow(x,y):
+    subprocess.run(['osascript', '-e', f'''
+    tell application "System Events"
+        set frontApp to name of first application process whose frontmost is true
+        tell process frontApp
+            set frontWindow to first window
+            set size of frontWindow to {x, y} -- New size for the window
+        end tell
+    end tell'''])
+
+moveWindow(0,0)
+
+def on_activate_left():
+    moveWindow(0,0)
+
+def on_activate_right():
+    moveWindow(width/2, 0)
+
+with keyboard.GlobalHotKeys({
+        '<cmd>+<shift_l>+a': on_activate_left,
+        '<cmd>+<shift_l>+d': on_activate_right}) as h:
+    h.join()
